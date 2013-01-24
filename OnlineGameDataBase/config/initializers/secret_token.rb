4ce216f5b8847a817e375c8740d84709badc8489
SirJaySaeba <sirjaysan@gmx.de>
2013-01-24 15:33:52 +0100

    Basic Projectfiles

---
+++ OnlineGameDataBase/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnlineGameDataBase::Application.config.secret_token = '88f4c79b5318db2cefde2c7c0be2454beb8190579bcb5a7678ca20f745f5d2221a6f0c6fb0380f4493b7960cfc8a10ef70f1ff0468eb0540ca7606ee26e00c49'
