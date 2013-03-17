ceeed298437280525265737d1c82709cfed9b2ba
SirJaySaeba <sirjaysan@gmx.de>
2013-03-17 10:24:50 +0100

    Layout and signup

--- Videospiel_Datenbank/config/routes.rb
+++ Videospiel_Datenbank/config/routes.rb
@@ -1,8 +1,7 @@
 VideospielDatenbank::Application.routes.draw do
+  get "users/new"
   root to: 'pages#home'
-
-  get "pages/home"
-
+  get "signup" => "users#new", as: "signup"     #  match '/signup',  to: 'users#new'
   get "pages/profile"
 
  
