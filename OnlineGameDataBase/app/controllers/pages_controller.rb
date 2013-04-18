321db48358082a983e80fc6ae736485e3dfe7f42
SirJaySaeba <sirjaysan@gmx.de>
2013-04-18 11:18:53 +0200

    game_id passed!?

--- OnlineGameDataBase/app/controllers/pages_controller.rb
+++
@@ -1,4 +0,0 @@
-class PagesController < ApplicationController
-  def startscreen
-  end
-end
--- Videospiel_Datenbank/app/controllers/reviews_controller.rb
+++ Videospiel_Datenbank/app/controllers/reviews_controller.rb
@@ -5,9 +5,9 @@ class ReviewsController < ApplicationController
   end
   
   def create
-    @game = Game.find_by_id(params[:id])
+    @game = Game.find_by_id(params[:game_id])
     @review = current_user.reviews.build(params[:review])
-    #@review.game_id = @game.id
+    @review.game_id = @game.id
     @review.user_id = current_user.id
     #@review = @game.reviews.build(params[:review])
     #@review.game_id = Game.find_by_id(params[:game_id])
@@ -23,7 +23,8 @@ class ReviewsController < ApplicationController
 
   def new
     @review = Review.new(params[:review])
-
+    @game = Game.find_by_id(params[:game_id])
+    @review.game_id = @game.id
   end
   
   def show
